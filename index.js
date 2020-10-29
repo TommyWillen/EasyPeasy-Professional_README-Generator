2e0fa393c69dcfec214c44b370f3e4f1e6508d7b
Tommy Willen <tommyallen1215@gmail.com>
2020-10-29 11:10:45 -0700

    initial commit and added gitignore file

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require("inquirer")
+const fs = require("fs");
+// This is an open source application and welcome for contribution. If you would like to contribute, simply fork my repository:
+
+
+// [Insert how to fork repo information here]
+
+
+// Contributions are protected by the contributor covenant V2.0. If you have any new features you would like to see added or want to report abuse please contact me at [INSERT CONTACT METHOD] 
+// Click [here](./contributor-covenant) for more information regarding contributor covenant V2.0.
