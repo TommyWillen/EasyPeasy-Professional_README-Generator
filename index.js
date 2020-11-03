7c4d3b27d03c0352efe2d241d7f95bea6697a8bd
Tommy Willen <tommyallen1215@gmail.com>
2020-11-03 10:12:46 -0800

    added inquirer to project, fixed bugs, created readme using the app, added images and gifs to readme

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 const inquirer = require("inquirer")
 const fs = require("fs");
-const Choices = require("inquirer/lib/objects/choices");
 
 inquirer.prompt([
     {
         type: "input",
         message: "What is your GitHub username?",
-        name: "github"
+        name: "github",
+        validate: input => input.length > 2,
     },
     {
         type: "input",
         message: "What is your email address?",
-        name: "email"
+        name: "email",
+        validate: input => input.length > 2,
     },
     {
         type: "input",
         message: "What is the name of your project?",
-        name: "title"
+        name: "title",
+        validate: input => input.length > 2,
     },
     {
         type: "input",
         message: "Please provide a description of your project",
-        name: "description"
+        name: "description",
+        validate: input => input.length > 2,
     },
     {
         type: "list",
         message: "What kind of License should your project have",
-        choices: ["Apache 2.0", "GNU v3.0", "MIT", "Bsd 2-clause", "Creative Commons Zero", "Other"],
+        choices: ["Apache 2.0", "GNU v3.0", "MIT", "BSD 3-clause", "Creative Commons Zero", "Other"],
         name: "license",
     },
     {
         type: "input",
         message: "What command should be run to install dependencies?",
-        name: "install"
+        name: "install",
     },
     {
         type: "input",
@@ -42,7 +45,8 @@ inquirer.prompt([
     {
         type: "input",
         message: "What does the user need to know regarding the usage of this repo?",
-        name: "usage"
+        name: "usage",
+        validate: input => input.length > 2,
     },
     {
         type: "confirm",
@@ -61,7 +65,7 @@ inquirer.prompt([
         message: "What does the user need to know about contributing to the repo?",
         name: "openSourceMsg",
         when: answers => answers.contributorCovenant === false,
-        // contributor covenant false other information
+        validate: input => input.length > 2,
     },
     {
         type: "confirm",
@@ -72,65 +76,83 @@ inquirer.prompt([
     },
     {
         type: "input",
-        message: "What dies the authorized user need to know about contributing to the repo?",
+        message: "What does the authorized user need to know about contributing to the repo?",
         name: "contributingClosed",
         when: answers => answers.closedSource === true,
+        validate: input => input.length > 2,
     }
 ]).then(response => {
-    console.log(response)
+    // console.log(response)
+    let { github, email, title, description, license, install, test, usage, openSource, contributorCovenant, openSourceMsg, closedSource, contributingClosed } = response;
+    // console.log(contributingClosed);
+    // console.log(contributingClosed ? "this should if closed" : "success if no closed comment!")
+    let licenseBadge = (response.license === "Apache 2.0") ? "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" :
+        (license === "GNU v3.0") ? "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)" :
+            (license === "MIT") ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" :
+                (license === "BSD 3-clause") ? "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)" :
+                    (license === "Creative Commons Zero") ? "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)" : " ";
+
+    const contributingBadge = contributorCovenant ? "[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md)" : " ";
+
+    const contributorCovenantMsg = contributorCovenant ? `This is an open source application and welcome for contribution. If you would like to contribute, you can fork my repo and submit any pull request for any features you would like added.    
+Contributions are protected by the contributor covenant V2.0. If you have any new features you would like to see added or want to report abuse please contact me at ${email} 
+Click [here](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md) for more information regarding contributor covenant V2.0.`: "";
+
+    if (!openSourceMsg) {
+        openSourceMsg = " "
+    }
+    if (!contributingClosed) {
+        contributingClosed = " "
+    }
+
     const readme = `# ${title}
 
-    ${licenseBadge}
-    ${contributingBadge}
+${licenseBadge}
+${contributingBadge}
+
+## Description
+
+${description}
+
+## Table of Contents
     
-    ## Table of Contents
+- [Description](#description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
     
-    - [Description](#description)
-    - [Installation](#Installation)
-    - [Usage](#Usage)
-    - [License](#License)
-    - [Contributing](#Contributing)
-    - [Tests](#Tests)
-    - [Questions](#Questions)
+## Installation
+    
+${install}
+    
+## Usage
+    
+${usage}
+    
+## License
+    
+The project is covered under the ${license} license.
+    
+## Contributing
     
     
-    ## Description
+${openSourceMsg}
+${contributorCovenantMsg}
+${contributingClosed}
     
-    ${description}
+## Tests
     
-    ## Installation
+${test}
     
-    ${install}
+## Questions
     
-    ## Usage
-    
-    ${usage}
-    
-    ## License
-    
-    The project is covered under the ${license} license.
-    
-    ## Contributing
-    
-    ${openSource}
-    ${openSourceMsg}
-    ${contributorCovenant}
-    ${contributingClosed}
-    
-    ## Tests
-    
-    ${test}
-    
-    ## Questions
-    
-    GitHub Profile: ${github}
-    Email: ${email}`
+GitHub Profile: [${github}](https://github.com/${github})
+
+Email: ${email}`
+
+    // console.log(readme);
+    fs.writeFile("README.md", readme, err => err ? console.log(err) : console.log("Success"));
 })
-// This is an open source application and welcome for contribution. If you would like to contribute, simply fork my repository:
-
-
-// [Insert how to fork repo information here]
-
-
-// Contributions are protected by the contributor covenant V2.0. If you have any new features you would like to see added or want to report abuse please contact me at [INSERT CONTACT METHOD] 
-// Click [here](./contributor-covenant) for more information regarding contributor covenant V2.0.
